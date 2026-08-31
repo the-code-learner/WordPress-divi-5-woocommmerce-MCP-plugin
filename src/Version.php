6e0f29a4bc39e25d49e4c1be87beaeb1c0df4ab6
The Code Learner <142033899+the-code-learner@users.noreply.github.com>
2026-09-01 00:58:33 +0200

    release: bump plugin version to 1.2.0

--- src/Version.php
+++ src/Version.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 namespace CodeLearner\Divi5WooCommerceMCP;
 
 final class Version {
-	public const NUMBER = '1.1.1';
+	public const NUMBER = '1.2.0';
 
 	private function __construct() {
 	}
